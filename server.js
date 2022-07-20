8f5d107177047b3fcc1b1970d15b1b3f4b278cf0
Khanh-T-Tran <khanhtuantran01@gmail.com>
2022-07-19 17:43:23 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,98 +1,15 @@
 const express = require('express');
-const routes = require('./routes');
+const sequelize = require('./config/connection');
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-
-app.use(routes);
-
-app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const mysql = require('mysql2');
-
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   database: 'todos_db',
-//   password: 'password',
-// });
-
-
-// const PORT = 3001;
-
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended :true}));
-
-
-// app.get('/api/todos', (req, res) => {
-  
-//   connection.query('SELECT * FROM todos;', (err, rows) => {
-//     if (err) {
-//       return res.status(400).json(err);
-//     }
-
-//     console.log("**************I AM ROWS****************")
-//     console.log(rows)
-//     res.json(rows);
-//   });
-// });
-
-
-// app.listen(PORT, () => console.log('Success!!!')); 
+app.use(express.urlencoded({ extended: true }));
+
+// Every time our server restarts, our data will automatically be deleted
+// and our tables will be recreated
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App running on PORT ${PORT}`));
+});
